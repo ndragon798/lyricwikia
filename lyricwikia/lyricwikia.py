af33fdbf70016d0502fac72641ba8c90e6cd6a08
Nathan Easton <ndragon798@yahoo.com>
2016-12-09 17:58:42 -0500

    Fixed unicode errors when parsing using ascii

--- lyricwikia/lyricwikia.py
+++ lyricwikia/lyricwikia.py
@@ -1,3 +1,4 @@
+
 from bs4 import BeautifulSoup
 import requests
 
@@ -5,20 +6,23 @@ __BASE_URL__ = 'http://lyrics.wikia.com'
 
 def urlize(string):
     """Convert string to LyricWikia format"""
+
     return '_'.join(string.title().split())
 
 def create_url(artist, song):
     """Create the URL in the LyricWikia format"""
+    artist = artist.encode("utf-8")
+    song = song.encode("utf-8")
     return __BASE_URL__ + '/wiki/{artist}:{song}'.format(
             artist=urlize(artist), song=urlize(song))
 
 def get_lyrics(artist, song, linesep='\n'):
     """Retrieve the lyrics of the song"""
     url = create_url(artist, song)
+    url = unicode(url, "utf-8", errors="ignore")
     response = requests.get(url)
     soup = BeautifulSoup(response.content, "html.parser")
     lyricbox = soup.find('div', {'class': 'lyricbox'})
-
     if not lyricbox:
         raise Exception('Cannot download lyrics')
     for br in lyricbox.findAll('br'):
